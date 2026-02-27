d963fea55b498202a7c68285c9c9dbc7cbce1c1a
Dhruv Naik <dhruvrnaik07@gmail.com>
2026-02-27 17:02:34 +0000

    added save_top_k functionality

--- src/hepattn/callbacks/checkpoint.py
+++ src/hepattn/callbacks/checkpoint.py
@@ -5,14 +5,14 @@ from lightning.pytorch.callbacks import ModelCheckpoint
 
 
 class Checkpoint(ModelCheckpoint):
-    def __init__(self, monitor: str = "val/loss", log_model: bool = True, **kwargs) -> None:
+    def __init__(self, monitor: str = "val/loss", log_model: bool = True, save_top_k: int=-1, **kwargs) -> None:
         filename = "epoch={epoch:03d}-" + monitor.replace("/", "_") + "={" + monitor + ":.5f}"
-        super().__init__(save_top_k=-1, monitor=monitor, filename=filename, auto_insert_metric_name=False, **kwargs)
+        super().__init__(save_top_k=save_top_k, monitor=monitor, filename=filename, auto_insert_metric_name=False, **kwargs)
         self.log_model = log_model
 
     def setup(self, trainer: Trainer, pl_module: LightningModule, stage: str) -> None:
         super().setup(trainer=trainer, pl_module=pl_module, stage=stage)
-        self.save_last = False
+        #self.save_last = False
         self.name = pl_module.name
         if stage == "fit":
             if trainer.fast_dev_run:
